b25665a9603aee72186a2751860baeed09fc7116
Gala Thevenet <galathevenet@hotmail.com>
2022-03-01 16:09:55 +0100

    controller and views created for recap_and_save/walks

---
+++ app/controllers/recap_and_save/walks_controller.rb
@@ -0,0 +1,9 @@
+class RecapAndSave::WalksController < ApplicationController
+  def show
+    # j'affiche la synthèse de ma walk et proposition de sauvegarder ou de la supprimer
+  end
+
+  def edit
+    # Si j'ai décidé de sauvegarder ma walk, j'ajoute une desc, des photos etc
+  end
+end
