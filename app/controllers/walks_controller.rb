ac88c11be2a30d19539ddf8aa86ef9a68e29a4ea
Gala Thevenet <galathevenet@hotmail.com>
2022-03-01 15:55:57 +0100

    walks controller created

---
+++ app/controllers/walks_controller.rb
@@ -0,0 +1,25 @@
+class WalksController < ApplicationController
+  def index
+    # affichage map + carte new walk + grilles cartes balades
+  end
+
+  def new
+    # page avant la création d'un nouvel itinéraire
+  end
+
+  def create
+    # création de la walk quand on appuie sur le bouton Go de description/walks
+  end
+
+  def edit
+    # en tant qu'utilisateurice je peux modifier la desc de mes itinéraires (notamment published/unpublished)
+  end
+
+  def update
+    # appuie sur bouton fin de la walk à la fin de mon parcours
+  end
+
+  def destroy
+    # en tant qu'utilisateurice je peux destroy un de mes itinéraires
+  end
+end
