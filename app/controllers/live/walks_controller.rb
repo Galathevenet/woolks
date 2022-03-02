a03f7b603e61a8630c6a8ddbb192631faab6b77b
Cedric Louyot <cedric.louyot@gmail.com>
2022-03-02 16:41:04 +0100

    Fix Live Walks

--- app/controllers/live/walks_controller.rb
+++ app/controllers/live/walks_controller.rb
@@ -1,5 +1,6 @@
 class Live::WalksController < ApplicationController
   def show
     # affichage de la carte en direct pour une walk avec ou non l'itinéraire + bouton stop
+    @walk = Walk.find(params[:id])
   end
 end
