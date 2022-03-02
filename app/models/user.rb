dd5410bbfd431bd55b9d8e06fc487da90e54635b
Gala Thevenet <galathevenet@hotmail.com>
2022-03-02 12:22:23 +0100

    added dependent destroy to reviews in Walk model and dependent nullify to reviews in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :walks
-  has_many :reviews
+  has_many :reviews, dependent: :nullify
 
   validates :username, presence: true, uniqueness: true
 end
