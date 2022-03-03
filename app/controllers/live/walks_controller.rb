b9ff79f0a1b2b260be9663029e9e262d8045af11
Cedric Louyot <cedric.louyot@gmail.com>
2022-03-03 13:28:54 +0100

    seed + various git issue corrected

--- app/controllers/live/walks_controller.rb
+++ app/controllers/live/walks_controller.rb
@@ -5,7 +5,7 @@ class Live::WalksController < ApplicationController
     # :id is the id of the current walk
     @walk = Walk.find(params[:id])
     @hotspots = Hotspot.all
-    @waypoints = @walk.original_walk.waypoints
+    @waypoints = @walk.waypoints
   end
 
   def update
