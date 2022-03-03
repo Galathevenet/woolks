a4b4a56d0c46f856737fa7b78d817e46cf9c2c73
Cedric Louyot <cedric.louyot@gmail.com>
2022-03-03 07:17:05 +0100

    comment added

--- app/controllers/live/walks_controller.rb
+++ app/controllers/live/walks_controller.rb
@@ -1,6 +1,8 @@
 class Live::WalksController < ApplicationController
   def show
     # affichage de la carte en direct pour une walk avec ou non l'itinéraire + bouton stop
+    
+    # :id is the id of the current walk
     @walk = Walk.find(params[:id])
     @hotspots = Hotspot.all
     @waypoints = @walk.original_walk.waypoints
