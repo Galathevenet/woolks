586394451ced2bdba9268695271ac77c2ca8ace6
Cedric Louyot <cedric.louyot@gmail.com>
2022-03-04 15:06:59 +0100

    map added

--- app/controllers/recap_and_review/walks_controller.rb
+++ app/controllers/recap_and_review/walks_controller.rb
@@ -2,6 +2,9 @@ class RecapAndReview::WalksController < ApplicationController
   def show
     # affichage de la synthèse de la walk + ajouter une review à l'itinéraire
     @walk = Walk.find(params[:id])
+    @hotspots = @walk.hotspots
+    @waypoints = @walk.waypoints
+    @start_points = [longitude: @waypoints.first.longitude, latitude: @waypoints.first.latitude]
     @review = Review.new
   end
 
