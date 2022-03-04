2d8c44ccfb74158d091712d63ef53e98e3cdf565
Cedric Louyot <cedric.louyot@gmail.com>
2022-03-04 13:43:48 +0100

    added data-mapbox-target

--- app/controllers/live/walks_controller.rb
+++ app/controllers/live/walks_controller.rb
@@ -6,14 +6,16 @@ class Live::WalksController < ApplicationController
     @walk = Walk.find(params[:id])
     @hotspots = Hotspot.all
     @waypoints = @walk.original_walk.waypoints
+    @start_points = [longitude: @waypoints.first.longitude, latitude: @waypoints.first.latitude]
   end
 
   def update
     @walk = Walk.find(params[:id])
     # From: "2.3522219,48.856614,2.3522219,48.856614,2.3522219,48.856614"
     # To: [[2.3522219, 48.856614], [2.3522219, 48.856614], [2.3522219, 48.856614]]
-    params['walk']['waypoints'].split(',').map(&:to_f).each_slice(2).to_a.each do |coord|
-      Waypoint.create!(walk: @walk, longitude: coord[0], latitude: coord[1])
+    params['walk']['waypoints'].split(',').map(&:to_f).each_slice(2).to_a.each do |coords|
+      console.log(coords)
+      Waypoint.create!(walk: @walk, longitude: coords[0], latitude: coords[1])
     end
     redirect_to recap_and_review_walk_path
   end
