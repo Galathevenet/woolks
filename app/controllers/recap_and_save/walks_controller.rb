26b4f217818f5935763cebadfa8b33430bedc01c
Carole Chene <chenecarole.pro@gmail.com>
2022-03-04 16:40:19 +0100

    form + map

--- app/controllers/recap_and_save/walks_controller.rb
+++ app/controllers/recap_and_save/walks_controller.rb
@@ -5,6 +5,9 @@ class RecapAndSave::WalksController < ApplicationController
 
   def edit
     # Si j'ai décidé de sauvegarder ma walk, j'ajoute une desc, des photos etc
-    @walk = Walk.find(params[:walk_id])
+    @walk = Walk.find(params[:id])
+    @hotspots = @walk.hotspots
+    @waypoints = @walk.waypoints
+    @start_points = [longitude: @waypoints.first.longitude, latitude: @waypoints.first.latitude]
   end
 end
