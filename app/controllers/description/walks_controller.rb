a5f65e3215bf6601d64a2f2aa577eb8875c80c03
Gala Thevenet <galathevenet@hotmail.com>
2022-03-04 12:26:32 +0100

    changed icone color for hotspots + carousel img size in modal defined

--- app/controllers/description/walks_controller.rb
+++ app/controllers/description/walks_controller.rb
@@ -2,5 +2,14 @@ class Description::WalksController < ApplicationController
   def show
     @walk = Walk.find(params[:id]) # Original walk
     @new_walk = Walk.new
+
+    @hps = []
+    @walk.hotspots.each do |h|
+      case h.category
+      when "dispenser" then @hps.push("fa-trash-can")
+      when "park" then @hps.push("fa-tree")
+      when "fountain" then @hps.push("fa-faucet")
+      end
+    end
   end
 end
