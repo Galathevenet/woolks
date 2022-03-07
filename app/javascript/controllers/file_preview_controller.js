cbfd7baac8ea60f7a08483a82922b7bc1a4442d6
Gala Thevenet <galathevenet@hotmail.com>
2022-03-07 15:18:09 +0100

    js fixed for preview of several pics in review

--- app/javascript/controllers/file_preview_controller.js
+++ app/javascript/controllers/file_preview_controller.js
@@ -1,23 +1,15 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
+
   static targets = ["field", "imageTemplate"]
 
-  connect() {
-    console.log('Hello, Stimulus!')
-    console.log(this)
-  }
-
   displayPhotoMiniature(event) {
-    console.log("coucou from photos")
-    console.log(event.target.files);
-
-    if (event.target.files) {
-        let file = event.target.files[0]
-        let blobUrl = URL.createObjectURL(file)
-        this.imageTemplateTarget.content.querySelector("img").src = blobUrl
-        const content = this.imageTemplateTarget.innerHTML
-        this.fieldTarget.insertAdjacentHTML ("beforeend", content)
-      }
-    }
+    Array.from(event.target.files).forEach((file) => {
+      let blobUrl = URL.createObjectURL(file)
+      this.imageTemplateTarget.content.querySelector("img").src = blobUrl
+      const content = this.imageTemplateTarget.innerHTML
+      this.fieldTarget.insertAdjacentHTML ("beforeend", content)
+    });
   }
+}
