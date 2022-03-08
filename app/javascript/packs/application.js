e6b9e10e6f123f95b393db2c419f81f6d718d506
Marion Santos <marionsantos@outlook.fr>
2022-03-08 17:47:07 +0100

    corrections

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,34 +14,3 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
-
-const addRemoveFavorite = (user, walk) => {
-  const heart = document.getElementById("fa-heart")
-  heart.addEventListener("click", (event) => {
-    console.log(event);
-    console.log(event.currentTarget);
-    // const user = current_user;
-    // const walk = document.querySelector(`#${walkId}`);
-    // if (user.favorited?(walk) {
-    //   user.unfavorite(walk)
-    // } else {
-    //   user.favorite(walk)
-    // }
-  })
-};
-
-addRemoveFavorite("", "");
-
-// methode hors stimulus
-
-//HTML
-//mettre une classe commune à tous les coeurs
-//
-
-//JS
-// Retrouver tous les cœurs de la page
-// Pour chaque cœur
-//     - mettre un écouteur au click
-//     - si cœur solid => enlever la classe solid et passer à vide
-//     - sinon inverse
-//     - Dire au serveur que le current_user a cliqué
